8603c1d9d50a745b09a7a76ef55a0036cdb44075
Samuel-Rendon-Loaiza <samuelrendonloaiza7@gmail.com>
2025-12-02 08:24:34 -0500

    feat: estructura inicial del proyecto

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// Función de suma
+export function suma(a, b) {
+  return a + b;
+}
+
+const resultado = suma(8, 3);
+console.log('Resultado:', resultado);
+
+// Mensaje en consola
+console.log("App lista");
