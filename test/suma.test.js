1c3df45b2cd6c75b04eee9dff147c30e56e3309a
Samuel-Rendon-Loaiza <samuelrendonloaiza7@gmail.com>
2025-12-02 08:52:33 -0500

    feat(ui): agregar boton y lista en index.html

--- test/suma.test.js
+++ test/suma.test.js
@@ -1,8 +1,8 @@
-import assert from 'assert';
-import { suma } from '../src/app.js';
-
-describe('suma()', () => {
-  it('debe sumar 2 + 3 = 5', () => {
-    assert.strictEqual(suma(2, 3), 5);
-  });
+import test from 'node:test';
+import assert from 'node:assert/strict';
+function suma(a, b) {
+  return a + b;
+}
+test('suma 2 + 3 = 5', () => {
+  assert.equal(suma(2, 3), 5);
 });
